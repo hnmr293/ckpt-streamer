70aec9b8e2cb461d02cbbc32d738f328b415d24c
hnmr <pute.tk@gmail.com>
2025-03-20 22:14:05 +0900

    windows はいったん諦め (^q^)

--- src/ckpt_streamer/mmap/win/mmap.py
+++ src/ckpt_streamer/mmap/win/mmap.py
@@ -5,5 +5,12 @@ import logging
 _logger = logging.getLogger(__name__)
 
 
-def free_memory():
+try:
+    from ctypes import windll as _windll, wintypes as _wintypes
+except:
+    _logger.exception("Failed to load windll")
+    raise
+
+
+def free_memory(start_ptr: int, end_ptr: int):
     raise NotImplementedError()
